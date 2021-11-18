6f63513e7c45ca30ed24a8ee597e1eb0e79a782d
Nivedhan <nivedhan1998@gmail.com>
2021-11-18 19:26:53 +0530

    git commit WebOrdersDeployment.py

--- WebOrdersDeployment.py
+++ WebOrdersDeployment.py
@@ -1,54 +1,64 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-import pycaret
+import openpyxl
+from sklearn.linear_model import LinearRegression
+import pickle
 
 concat = pd.read_excel('concat.xlsx')
+X = concat[['GA-Organicsearches','GA-Users']]
+Y = concat['Weborders']
 
-from pycaret.regression import *
-s = setup(concat, target = 'Weborders', transform_target = True, log_experiment = True, experiment_name = 'WEB')
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(X,Y, test_size = 0.2, random_state = 10)
 
-#Enable models
-models(internal=True)[['Name', 'GPU Enabled']]
+model = LinearRegression()
+model.fit(xtrain, ytrain)
+ypred = model.predict(xtest)
 
-# compare baseline models
-best = compare_models()
-print(best)
-
-et_model = create_model('et')
-predict_model(et_model)
-save_model(et_model, model_name = 'extra_tree_model')
+#Saving Model
+pickle_out = open("Weborders.pkl", mode="wb")
+pickle.dump(model, pickle_out)
+pickle_out.close()
 
 
-def predict_quality(model, df):
-    predictions_data = predict_model(estimator=model, data=concat)
-    return predictions_data['Label'][0]
+# loading the trained model
+pickle_in = open('Weborders.pkl', 'rb')
+Orders = pickle.load(pickle_in)
 
-model = load_model('extra_tree_model')
 
-st.title('Wine Quality Classifier Web App')
-st.write('This is a web app to classify the quality of your wine based on\
-         several features that you can see in the sidebar. Please adjust the\
-         value of each feature. After that, click on the Predict button at the bottom to\
-         see the prediction of the classifier.')
+def prediction(OrganicSearches, Users):
+    # Making predictions
+    prediction = Orders.predict([[OrganicSearches, Users]])
+    return prediction
 
-Organicsearch = st.sidebar.slider(label='OS', min_value=4.0,
-                                  max_value=16.0,
-                                  value=10.0,
-                                  step=0.1)
-Users = st.sidebar.slider(label='Users', min_value=0.00,
-                          max_value=2.00,
-                          value=1.00,
-                          step=0.01)
 
-features = {'GA-Organicsearches': Organicsearch, 'GA-Users': Users}
+# this is the main function in which we define our webpage
+def main():
+    # front end elements of the web page
+    html_temp = """ 
+    <div style ="background-color:yellow;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
+    </div> 
+    """
 
-features_df = pd.DataFrame([features])
+    # display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html=True)
 
-st.table(features_df)
+    # following lines create boxes in which user can enter data required to make prediction
+    Organicsearch = st.sidebar.slider(label='OS', min_value=4.0, max_value=16.0, value=10.0, step=0.1)
+    Users = st.sidebar.slider(label='Users', min_value=0.00, max_value=2.00, value=1.00, step=0.01)
 
-if st.button('Predict'):
-    prediction = predict_quality(model, features_df)
-    st.write(' Based on feature values, your wine quality is ' + str(prediction))
+    # when 'Predict' is clicked, make the prediction and store it
+    if st.button("Predict"):
+        result = prediction(OrganicSearches, Users)
+        st.success('Your loan is {}'.format(result))
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
+
+
 
 
